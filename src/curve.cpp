89da71bf8e91e72a77fc824f40958680ce947211
xi <tobias.bengfort@gmx.net>
2010-11-01 17:54:58 +0100

    clean

---
+++ src/curve.cpp
@@ -0,0 +1,54 @@
+#include "curve.h"
+
+#include <cmath>
+
+Curve::Curve() {
+  marker=new Marker();
+  sample=new Sample(marker);
+  seeker=0;
+  tempo=90;
+}
+
+Curve::~Curve() {
+  delete[] sample;
+  delete[] marker;
+}
+
+float Curve::get(float nn) {
+  // TODO interpolation?
+  return sample->get(nn);
+}
+
+float Curve::getSeeker() {return seeker;}
+void Curve::setSeeker(float nn) {
+  if (nn>=0 && nn<=1) seeker=nn;
+}
+
+void Curve::setTempo(int bpm) {tempo=bpm;}
+int Curve::getTempo() {return tempo;}
+
+// TODO select and move marker
+void Curve::addMarker() {
+  float n=marker->nnew2new(seeker);
+  marker->add(marker->new2old(n),n);
+}
+
+void Curve::removeMarker() {
+  int i=marker->getAreaNew(marker->nnew2new(seeker));
+  marker->remove(i);
+}
+
+int Curve::getMarkerLength() {return marker->getLength();}
+float Curve::getMarker(int i) {return marker->new2nnew(marker->getNew(i));}
+
+void Curve::print() {
+  for (int i=0; i<100; ++i) { // 100 or any number 
+    for (float j=-1; j<get(i); j+=0.1)
+      std::cout << "+";
+    if (std::abs(getSeeker()-i)<0.01)
+      std::cout << " *";
+    std::cout << std::endl;
+  }
+}
+
+
---
+++ src/curve.h
@@ -0,0 +1,47 @@
+#ifndef __XICURVE_H
+#define __XICURVE_H
+
+#include <iostream>
+#include "sample.h"
+
+// main part of the gui
+// layer between screen and backend. Converts everything to screen (0-1) values
+class Curve {
+public:
+  Curve(); 
+  ~Curve();
+  float getSeeker();
+  void setSeeker(float nn);
+  void setTempo(int bpm);
+  int getTempo();
+  void print();
+  // direct access to sample
+  Sample* sample;
+  // indirect access to sample
+  float get(float nn);
+  // indirect access to marker
+  void addMarker();
+  void removeMarker();
+  int getMarkerLength();
+  float getMarker(int i);
+private:
+  float seeker; // 0-1
+  Marker* marker;
+  int tempo; // bpm
+};
+
+/*
+int main() {
+  Marker* m=new Marker();
+  Sample* s=new Sample(m);
+  m->add(0.5,0.3);
+  int error;
+  error=s->loadFile("../../test.wav");
+  
+  Curve c(s);
+  c.setScreenWidth(100);
+  c.print();
+}
+*/
+
+#endif
