94e7a516f10b45032fbe9571530795ec35ecbb01
Tobias Bengfort <tobias.bengfort@gmx.net>
2010-11-07 18:13:51 +0100

    rb_update2

--- src/rbprocess.h
+++ src/rbprocess.h
@@ -13,15 +13,11 @@ this uses the rubberband library
 
 int RBprocess(int olength, float* data, Marker* marker, Sample* sample, int n=1) {
 
-/*  
+  int length=sample->getLength();
   int ostart=int(marker->getOld(0)*olength);
   if (ostart<0) ostart=0;
   int olength2=int(marker->getOld(marker->getLength()-1)*olength)-ostart;
   if (olength2>olength-ostart) olength2=olength-ostart;
-*/
-  
-  int ostart=0;
-  int olength2=olength;
 
   float **ibuf = new float *[1];
   ibuf[0]=new float[olength2];
@@ -32,39 +28,27 @@ int RBprocess(int olength, float* data, Marker* marker, Sample* sample, int n=1)
   float **obuf = new float *[1];
   obuf[0]=data;
 
-
-  RubberBand::RubberBandStretcher ts(44100, 1, 0, sample->getLength()/(float)olength2);
+  RubberBand::RubberBandStretcher ts(44100, 1, 0, 1);
   // map // TODO not precise
   std::map<unsigned int, unsigned int> fmap;
   for (int i=0; i<marker->getLength(); ++i) {
-    fmap[int(marker->getOld(i)*olength)-ostart]=int(marker->getNew(i)*sample->getLength());
-/*
-    // additional 
-    for (int j=1; j<n; ++j) {
-      float old=(marker->getOld(i+1)-marker->getOld(i))*j/(float)n;
-      fmap[int(old*olength)-ostart]=int(marker->new2nnew(marker->old2new(old))*sample->getLength());
-    }
-*/
+    float old= marker->getOld(i);
+    float nnew= marker->new2nnew(marker->old2new(old));
+    fmap[int(old*olength)-ostart]=int(nnew*length);
   }
   ts.setKeyFrameMap(fmap);
 
   sample->setFinished(0.03);
   ts.study(ibuf, olength2, true);
-  ts.setMaxProcessSize(olength2*2);
+  ts.setMaxProcessSize(olength2);
   int a1=-1;
   int a2=0;
   sample->setFinished(0.06);
-  while (a1!=a2) {
-    ts.process(ibuf, ts.getSamplesRequired(), false);
-    a1=a2;
-    a2=ts.available();
-    sample->setFinished(a2/(float)sample->getLength()*0.8+0.05); // TODO doesnt really work
-  }
-  sample->setFinished(0.8);
   ts.process(ibuf, olength2, true);
-  sample->setFinished(0.95);
+  sample->setFinished(0.9);
 
   int avail=ts.available();
+  if (avail>length) avail=length;
   ts.retrieve(obuf, avail);
 
   for (int j=0; j<avail; ++j) {
@@ -74,7 +58,7 @@ int RBprocess(int olength, float* data, Marker* marker, Sample* sample, int n=1)
     data[j] = value;
   }
 
-  for (int i=avail; i<sample->getLength(); ++i) {
+  for (int i=avail; i<length; ++i) {
     data[i]=0;
   }
 
